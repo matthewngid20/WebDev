3ed65d448dcf27ff363457841992f4073ead901b
matthewngid20 <matthew.ng.id20@gmail.com>
2021-06-18 11:05:58 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $date = 18;
+    $test = "Hello PHP";
+    $name = "Matthew";
+
+    //Concatenation of strings
+    echo "Hello, " . $name . " Today is " . $day;
+
+    //Output htmlwith double quotes
+    echo "<h1> $date <h1>";
+
+    //Output html with single quotes
+    echo '<p>' . $day . '<p>';
+
+?>
