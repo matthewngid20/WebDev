d55014050998d2744b5fbcd9e9901f6d8bd2cd19
matthewngid20 <matthew.ng.id20@gmail.com>
2021-06-18 11:28:20 +1000

    second commit

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-    $date = 18;
-    $test = "Hello PHP";
-    $name = "Matthew";
-
-    //Concatenation of strings
-    echo "Hello, " . $name . " Today is " . $day;
-
-    //Output htmlwith double quotes
-    echo "<h1> $date <h1>";
-
-    //Output html with single quotes
-    echo '<p>' . $day . '<p>';
-
+// command has to go between tags
+/* multiline
+ comments */
+ $name = "Johannes";
+ $day = 'Friday';
+ $date = 18;
+ $test = "Hello PHP";
+// concatenation of strings
+ echo "Hello, " . $name . " Today is " . $day;
+ // output html with double quotes
+echo "<h1>$date</h1>";
+// output html with single quotes
+echo '<p>' . $day . '</p>';
 ?>
+
+<div style="background-color:green;color:white;">
+    <?php echo $test ?>
+</div>
